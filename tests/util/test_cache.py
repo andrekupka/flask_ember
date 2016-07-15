413533e01e32457b034343a018fc8a7640c3858c
Andre Kupka <kupka@in.tum.de>
2016-07-15 09:25:57 +0200

    add test cases for cache

---
+++ tests/util/test_cache.py
@@ -0,0 +1,23 @@
+import unittest
+
+from flask_ember.util.cache import Cache
+
+
+class CacheTestCase(unittest.TestCase):
+    def test_basic(self):
+        cache = Cache(lambda x: x * 2)
+        self.assertEqual(cache.get(1), 2)
+
+    def test_caching(self):
+        calculations = 0
+
+        def cache_function(value):
+            nonlocal calculations
+            calculations += 1
+            return value * 2
+
+        function = Cache(cache_function)()
+
+        self.assertEqual(function(2), 4)
+        self.assertEqual(function(2), 4)
+        self.assertEqual(calculations, 1)
