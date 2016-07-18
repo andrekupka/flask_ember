009bb3ce919edaaae7fca5075fd08a48598c363d
Andre Kupka <kupka@in.tum.de>
2016-07-18 18:33:45 +0200

    remove some debugging output

--- flask_ember/database/declarative_model_meta.py
+++ flask_ember/database/declarative_model_meta.py
@@ -7,8 +7,6 @@ class DeclarativeModelMeta(DeclarativeMeta, ResourceMeta):
 
     def __new__(mcs, name, bases, attrs):
         print("\n" + (80 * '-') + "\nDECLARATIVE MODEL META: %s" % name)
-        print(bases)
-        print(attrs)
         # TODO We set this class to abstract if it is a resource and not a
         # model class in order to avoid sqlalchemy table generation. This is
         # kind of a dirty hack as a resource should not inherit from any model
