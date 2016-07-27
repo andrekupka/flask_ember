5ea88c33efac9ef5b6b9d6f268d42274a6a54a79
Andre Kupka <kupka@in.tum.de>
2016-07-27 16:27:50 +0200

    remove wrong comment from one to one relationship

--- flask_ember/resource/relationships/one_to_one.py
+++ flask_ember/resource/relationships/one_to_one.py
@@ -4,11 +4,6 @@ from .relationship_base import RelationshipBase
 
 class OneToOne(RelationshipBase):
     def __init__(self, target_kind, backref):
-        # TODO Fix the determination of the inverse side. Currently this is
-        # done automatically if the inverse relationship is not defined
-        # explicitly. Otherwise a foreign key will be generated on both sides
-        # and sqlalchemy will fail. Also have a look at
-        # create_inverse_relationship.
         super().__init__(target_kind, backref)
 
     def create_property_builder(self):
